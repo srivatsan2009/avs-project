92c6573e19b1703ba26b51074a9c7b08375803d7
srivatsan2009 <71771607+srivatsan2009@users.noreply.github.com>
2021-05-05 14:04:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,64 @@
-flag=0;
-
-function preload() {
-}
-
-
-function setup() {
-canvas=createCanvas(300,300);
+objects=[];
+status="";
+function setup()
+{
+canvas=createCanvas(500,400);
 canvas.center();
-video=createCapture(VIDEO,300,300);
+video=createCapture(VIDEO);
 video.hide();
 }
 
-function start() {
-objectdetector=ml5.objectDetector("cocossd",modeloaded);
-document.getElementById("status").innerHTML="status:detectin' objects";
+ function start()
+{
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="status : detecting objects";
+object_name=document.getElementById("object_name").value;
 }
-
-function modeloaded() {
-console.log("model loaded!");
+function modelLoaded()
+{
+console.log("model is loaded !");
 status=true;
-
-if(getvideo=="lamborghini_video") {
-video2.pause();
-video1.loop();
-flag=1;
 }
-else {
-video1.pause();
-video2.loop();
-flag=0;
+function gotResults(error,results)
+{
+if(error)
+{
+console.log(error);
 }
-}                               
+else
+{
+console.log(results);
+objects=results;
+}
+}
 
-function draw() {
-image(VIDEO,300,300);
+function draw()
+{
+image(video,0,0,500,400);
+if(status !="")
+{
+objectDetector.detect(video,gotResults);
+for(i=0;i<objects.length;i++)
+{
+document.getElementById("status").innerHTML="status : objects detected";
+fill('#ff0000');
+percent=floor(objects[i].confidence*100);
+text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+noFill();
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+if(objects[i].label==object_name)
+{
+video.stop();
+objectDetector.detect(gotResults);
+document.getElementById("object_status").innerHTML=object_name +" found";
+synth=window.speechSynthesis;
+utterThis=new SpeechSynthesisUtterance(object_name+" found");
+synth.speak(utterThis);
+}
+else
+{
+document.getElementById("object_status").innerHTML=object_name+" not found";
+}
+}
+}
 }
